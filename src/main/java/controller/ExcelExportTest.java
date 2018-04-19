4449441d69c03549c9911e9588ff98c4633d8d98
Cheung <550690513@qq.com>
2018-04-19 10:53:05 +0800

    新增测试图例;补充部分注释,便于阅读

--- src/main/java/controller/ExcelExportTest.java
+++ src/main/java/controller/ExcelExportTest.java
@@ -14,16 +14,22 @@ import java.util.LinkedHashMap;
 
 /**
  * Created by Cheung on 2017/12/19.
+ *
+ * @author Cheung
+ * @version 2.0.0
+ * @date 2018/4/19
  */
 public class ExcelExportTest {
 
 	public static void main(String[] args) throws IOException {
-		// 模拟10W条数据
-		int count = 100000;
-	 	JSONArray studentArray = new JSONArray();
+		// 模拟100W条数据,存入JsonArray,此处使用fastJson(号称第一快json解析)快速解析大数据量数据
+		// 至于容量问题,Java数组的length必须是非负的int，所以它的理论最大值就是java.lang.Integer.MAX_VALUE = 2^31-1 = 2147483647。
+		// 由于xlsx最大支持行数为1048576行,此处模拟了1048573调数据,剩下的3条占用留给自定义的excel的头信息和列项.
+		int count = 1048573;
+		JSONArray studentArray = new JSONArray();
 		for (int i = 0; i < count; i++) {
 			Student s = new Student();
-			s.setName("POI" + i);
+			s.setName("POI-" + i);
 			s.setAge(i);
 			s.setBirthday(new Date());
 			s.setHeight(i);
@@ -32,10 +38,15 @@ public class ExcelExportTest {
 			studentArray.add(s);
 		}
 
+		/*
+		 * titleList存放了2个元素,分别为titleMap和headMap
+		 */
 		ArrayList<LinkedHashMap> titleList = new ArrayList<LinkedHashMap>();
+		// 1.titleMap存放了该excel的头信息
 		LinkedHashMap<String, String> titleMap = new LinkedHashMap<String, String>();
-		titleMap.put("title1","POI导出大数据量Excel Demo");
-		titleMap.put("title2","https://github.com/550690513");
+		titleMap.put("title1", "POI导出大数据量Excel Demo");
+		titleMap.put("title2", "https://github.com/550690513");
+		// 2.headMap存放了该excel的列项
 		LinkedHashMap<String, String> headMap = new LinkedHashMap<String, String>();
 		headMap.put("name", "姓名");
 		headMap.put("age", "年龄");
@@ -43,22 +54,22 @@ public class ExcelExportTest {
 		headMap.put("height", "身高");
 		headMap.put("weight", "体重");
 		headMap.put("sex", "性别");
+
 		titleList.add(titleMap);
 		titleList.add(headMap);
 
 
-
 		File file = new File("D://ExcelExportDemo/");
-		if (!file.exists()) file.mkdir();// 创建该文件夹目录
+		if (!file.exists()) file.mkdirs();// 创建该文件夹目录
 		OutputStream os = null;
-		Date date = new Date();
 		try {
-			// .xlsx格式
-			os = new FileOutputStream(file.getAbsolutePath() + "/" + date.getTime() + ".xlsx");
 			System.out.println("正在导出xlsx...");
+			long start = System.currentTimeMillis();
+			// .xlsx格式
+			os = new FileOutputStream(file.getAbsolutePath() + File.separator + start + ".xlsx");
 			ExcelUtil.exportExcel(titleList, studentArray, os);
-			System.out.println("导出完成...共" + count + "条数据,用时" + (System.currentTimeMillis() - date.getTime()) + "ms");
-			System.out.println("文件路径：" + file.getAbsolutePath() + "/" + date.getTime() + ".xlsx");
+			System.out.println("导出完成...共" + count + "条数据,用时" + (System.currentTimeMillis() - start) + "毫秒");
+			System.out.println("文件路径：" + file.getAbsolutePath() + File.separator + start + ".xlsx");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
